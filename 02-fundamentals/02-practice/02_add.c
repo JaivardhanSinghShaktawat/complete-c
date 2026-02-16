8ed744e7377e5bb6861ef9db02f3ef7baace71b2
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2026-02-17 00:42:40 +0530

    Day 1 [Fundamentals]: Add two numbers program

---
+++ 02-fundamentals/02-practice/02_add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(int argc , char const *argv[])
+{
+    int a , b ; 
+    printf("Enter number a :\n"); 
+    scanf("%d",&a); // taking input
+    printf("Enter number b :\n"); 
+    scanf("%d",&b); // taking input
+
+    printf("The sum is %d\n",a+b); 
+    
+    return 0 ; 
+}
