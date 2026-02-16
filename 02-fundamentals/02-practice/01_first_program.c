91ba950ede4ccf8ed5389ef106fdab9e2f2c1ed9
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2026-02-17 00:40:47 +0530

    Day 1 [Fundamentals]: First program

---
+++ 02-fundamentals/02-practice/01_first_program.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(int argc , char const* argv[])
+{
+  printf("Hello World\n"); // printing Hello World with new line in standard output
+  return 0 ; // returning 0 value to the operating system.
+}
